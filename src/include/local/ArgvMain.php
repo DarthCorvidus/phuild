5d02ca88b4cc81bc13e703c80251524392e7ebdd
DarthCorvidus <github@vm01.telton.de>
2019-08-28 11:12:44 +0200

    Added ArgvMain with boolean parameters --source and --require.

---
+++ src/include/local/ArgvMain.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ArgvMain
+ *
+ * @author hm
+ */
+class ArgvMain implements ArgvModel{
+	public function getArgModel(int $arg): \ArgModel {
+		
+	}
+
+	public function getBoolean(): array {
+		return array("source", "require");
+	}
+
+	public function getParamCount(): int {
+		return 0;
+	}
+
+}
